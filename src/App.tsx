f128bf34e18c1e70bdb4220095c17e001eb3b729
Emmanuel Munoz <emunozce80@gmail.com>
2024-05-27 17:45:32 -0600

    refactor: Remove Amplify auth and data resources

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import { signOut } from 'aws-amplify/auth';
+
 function App() {
     return (
         <>
-            <h1 className="text-3xl font-bold text">Hola Puto Iker</h1>
+            <div>
+                <Authenticator>
+                    {({ signOut, user }) => (
+                        <button onClick={signOut}>Sign Out</button>
+                    )}
+                    <h1 className="text-3xl font-bold text">Hola Puto Iker</h1>
+                </Authenticator>
+            </div>
         </>
     );
 }
