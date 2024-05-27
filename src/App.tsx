12ad80b255c70adccda636198d23dc18717ba18f
Emmanuel Munoz <emunozce80@gmail.com>
2024-05-27 12:48:39 -0600

    refactor: Update App title to use Spanish greeting

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
     return (
         <>
-            <h1 className="text-3xl font-bold text">Hello world!</h1>
+            <h1 className="text-3xl font-bold text">Hola Puto Iker</h1>
         </>
     );
 }
