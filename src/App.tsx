621c2d586b0eeb10a1adf08e6a0f7e93bafdc8d5
Emmanuel Munoz <emunozce80@gmail.com>
2024-05-31 12:56:05 -0600

    chore: Update .gitignore and package.json, and remove Amplify auth and data resources

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
-import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
-import { signOut } from 'aws-amplify/auth';
-
 function App() {
     return (
         <>
-            <div>
-                <Authenticator>
-                    {({ signOut, user }) => (
-                        <button onClick={signOut}>Sign Out</button>
-                    )}
-                    <h1 className="text-3xl font-bold text">Hola Puto Iker</h1>
-                </Authenticator>
-            </div>
+            <h1 className="text-3xl font-bold text">Hola Puto Iker</h1>
         </>
     );
 }
