219e75ba18840d0e3e4aff57c3d38a7144af3ecf
Emmanuel Munoz <emunozce80@gmail.com>
2024-05-31 15:31:53 -0600

    feat: Add Loader component for displaying loading state

--- src/components/Register_Sale_Page.tsx
+++ src/components/Register_Sale_Page.tsx
@@ -1,4 +1,4 @@
-// import axios from 'axios';
+import axios from 'axios';
 import {
     Card,
     CardHeader,
@@ -7,109 +7,163 @@ import {
     Button,
     Spacer,
 } from '@nextui-org/react';
-// import { useForm } from 'react-hook-form';
-// import { useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Loader from './Loader';
 
-// interface SalesData {
-//     name: string;
-//     quantity: number;
-//     price: number;
-//     store_id: number;
-// }
+interface SalesData {
+    product: string;
+    price: number;
+    quantity: number;
+}
 
-// interface ErrorData {
-//     message: string;
-// }
+interface ErrorData {
+    message: string;
+}
 
 export default function Register_Sale_Page() {
-    // const [isInvalid, setIsInvalid] = useState<ErrorData | null>(null);
-    // const {
-    //     register,
-    //     handleSubmit,
-    //     formState: { errors },
-    // } = useForm<SalesData>({
-    //     defaultValues: {
-    //         name: '',
-    //         quantity: 0,
-    //         price: 0,
-    //         store_id: 1, // Sustituir por el id del usuario logueado
-    //     },
-    // });
+    const [isLoading, setIsLoading] = useState(false);
+    const [isInvalid, setIsInvalid] = useState<ErrorData | null>(null);
+    const navigate = useNavigate();
 
-    // const onSubmit = handleSubmit(async (data) => {
-    //     try {
-    //         const response = await axios.post('', data);
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<SalesData>({
+        defaultValues: {
+            product: '',
+            price: 0,
+            quantity: 0,
+        },
+    });
 
-    //         if (response.status === 201) {
-    //             navigate('/sales-report');
-    //         }
-    //     } catch (error) {
-    //         if (axios.isAxiosError(error)) {
-    //             setIsInvalid(error.response?.data);
-    //         }
-    //     }
-    // });
+    const onSubmit = handleSubmit(async (data) => {
+        const jsonUserData = {
+            product: data.product,
+            price: data.price,
+            quantity: data.quantity,
+            total: data.price * data.quantity,
+            store_id: 1,
+        };
+
+        console.log('jsonUserData: ', jsonUserData);
+
+        try {
+            setIsLoading(true);
+            await axios.post(`endpoint/signup`, jsonUserData, {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            });
+            setTimeout(() => {
+                setIsLoading(false);
+                navigate;
+            }, 1000);
+        } catch (error) {
+            if (axios.isAxiosError(error)) {
+                setIsInvalid(error?.response?.data);
+            }
+        }
+    });
+
     return (
         <>
-            <div className="flex justify-center items-center">
-                <Card className="w-10/12 md:w-7/12 lg:w-5/12 my-32 sm:mb-0 md:mt-60 md:mb-24 lg:mb-4">
-                    <CardHeader>
-                        <h4 className="text-3xl text-green-500 font-bold hover:cursor-default">
-                            Make a Sale
-                        </h4>
-                    </CardHeader>
-                    <CardBody>
-                        {/* {isInvalid && (
-                        <p className="text-red-600">{isInvalid?.message}</p>
-                    )} */}
-                        <Spacer y={3} />
-                        <form
-                            // onSubmit={onSubmit}
-                            className="flex flex-col justify-center items-center"
-                            noValidate
-                        >
-                            <Input
-                                // {...register('email', {
-                                //     required: 'Email is required',
-                                //     pattern: {
-                                //         value: /^\S+@\S+\.\S+$/,
-                                //         message: 'Invalid email',
-                                //     },
-                                // })}
-                                // isInvalid={errors.email ? true : false}
-                                // errorMessage={errors.email?.message}
-                                type="text"
-                                label="Item"
-                                isClearable={true}
-                                labelPlacement="outside"
-                            />
-
-                            <Spacer y={6}></Spacer>
-                            <Input
-                                type="number"
-                                label="Price"
-                                placeholder="0.00"
-                                labelPlacement="outside"
-                                startContent={
-                                    <div className="pointer-events-none flex items-center">
-                                        <span className="text-default-400 text-small">
-                                            $
-                                        </span>
-                                    </div>
-                                }
-                            />
-                            <Spacer y={6}></Spacer>
-                            <Button
-                                type="submit"
-                                className="w-4/12 bg-green-500 font-semibold mb-4"
+            {isLoading ? (
+                <div className="my-32 sm:mb-0 md:mt-60 md:mb-10 lg:mb-0">
+                    <Loader />
+                </div>
+            ) : (
+                <div className="flex justify-center items-center">
+                    <Card className="w-10/12 md:w-7/12 lg:w-5/12 my-24 sm:mt-20 sm:mb-0 md:mt-40 md:mb-10">
+                        <CardHeader>
+                            <h4 className="text-3xl text-green-500 font-bold hover:cursor-default">
+                                Make a Sale
+                            </h4>
+                        </CardHeader>
+                        <CardBody>
+                            {isInvalid && (
+                                <p className="text-red-600">
+                                    {isInvalid?.message}
+                                </p>
+                            )}
+                            <Spacer y={3} />
+                            <form
+                                onSubmit={onSubmit}
+                                className="flex flex-col justify-center"
+                                noValidate
                             >
-                                Register
-                            </Button>
-                        </form>
-                    </CardBody>
-                </Card>
-            </div>
+                                <Input
+                                    {...register('product', {
+                                        required: 'Product name is required',
+                                        pattern: {
+                                            value: /^[A-Za-z]/i,
+                                            message: 'Invalid product name.',
+                                        },
+                                    })}
+                                    isInvalid={errors.product ? true : false}
+                                    errorMessage={errors.product?.message}
+                                    type="text"
+                                    label="Product name"
+                                    labelPlacement="outside"
+                                />
+                                <Spacer y={2}></Spacer>
+                                <div className="flex flex-row">
+                                    <Input
+                                        {...register('price', {
+                                            required: 'Price is required',
+                                            min: {
+                                                value: 0.01,
+                                                message:
+                                                    'Should be more than $0.00.',
+                                            },
+                                        })}
+                                        isInvalid={errors.price ? true : false}
+                                        errorMessage={errors.price?.message}
+                                        type="number"
+                                        label="Price"
+                                        placeholder="0.00"
+                                        labelPlacement="outside"
+                                        startContent={
+                                            <div className="pointer-events-none flex items-center">
+                                                <span className="text-default-400 text-small">
+                                                    $
+                                                </span>
+                                            </div>
+                                        }
+                                    />
+                                    <Spacer x={2} />
+                                    <Input
+                                        {...register('quantity', {
+                                            required: 'Quantity is required',
+                                            min: {
+                                                value: 1,
+                                                message:
+                                                    'Should be more than 0.',
+                                            },
+                                        })}
+                                        isInvalid={
+                                            errors.quantity ? true : false
+                                        }
+                                        errorMessage={errors.quantity?.message}
+                                        type="number"
+                                        label="Quantity"
+                                        labelPlacement="outside"
+                                    />
+                                </div>
+                                <Spacer y={6}></Spacer>
+                                <Button
+                                    type="submit"
+                                    className="self-center w-4/12 bg-green-500 font-semibold mb-4"
+                                >
+                                    Register Sale
+                                </Button>
+                            </form>
+                        </CardBody>
+                    </Card>
+                </div>
+            )}
         </>
     );
 }
